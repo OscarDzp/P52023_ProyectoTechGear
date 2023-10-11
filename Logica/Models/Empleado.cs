3307c01f4c6d850c587808a9b3e60eea4f8f8dfa
OscarDzp <ozumbado2000@gmail.com>
2023-10-11 10:53:26 -0600

    Add project files.

---
+++ Logica/Models/Empleado.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Models
+{
+    public class Empleado
+    {
+        //Propiedades de la clase
+
+        public int EmpleadoID { get; set; }
+
+        public string Nombre { get; set;}
+
+        public string Telefono { get; set;}
+
+        public string CorreoElectronico { get; set;}
+
+        public string Cargo { get; set;}
+
+        public string Contrasennia { get; set;}
+
+        //Listar para empleados
+        public DataTable Listar()
+        { 
+            DataTable R = new DataTable();
+
+
+            return R;
+        }
+    }
+}
