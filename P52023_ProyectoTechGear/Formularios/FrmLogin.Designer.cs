552c2d5b38a04b0804ae095246b6a4d4afebbf30
jeffermora16 <jeffermora123@gmail.com>
2023-10-12 11:04:20 -0600

    para aca

--- P52023_ProyectoTechGear/Formularios/FrmLogin.Designer.cs
+++ P52023_ProyectoTechGear/Formularios/FrmLogin.Designer.cs
@@ -62,7 +62,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::P52023_ProyectoTechGear.Properties.Resources.OIG;
-            this.pictureBox1.Location = new System.Drawing.Point(103, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(195, 150);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
